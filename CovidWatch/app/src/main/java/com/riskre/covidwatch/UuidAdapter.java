38873244262524e44b6563da1d1d50cca03f1837
Zsombor Szabo <zsombor@gmail.com>
2020-03-17 16:12:11 +0200

    Add support for advertizing in scan response via AdvertiseData.addServiceData Modify service UUID to 16 bit one (0xC019)

---
+++ CovidWatch/app/src/main/java/com/riskre/covidwatch/UuidAdapter.java
@@ -0,0 +1,21 @@
+package com.riskre.covidwatch;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UuidAdapter {
+
+    public final byte[] getBytesFromUUID(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    public final UUID getUUIDFromBytes(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        long high = byteBuffer.getLong();
+        long low = byteBuffer.getLong();
+        return new UUID(high, low);
+    }
+}
