4268c52ecf42f4e0edbb6c49409e3d9188ba90b3
Nitin Kumar <nkumarcc@gmail.com>
2020-04-09 14:55:13 -0400

    Dropped footer

--- CovidWatch/app/src/main/java/org/covidwatch/android/MainActivity.kt
+++ CovidWatch/app/src/main/java/org/covidwatch/android/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
 
         // Passing each menu ID as a set of Ids because each
@@ -46,7 +45,6 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
         // TODO #15: COMMENTING OUT BECAUSE USING EMULATOR
         // BRING BACK AFTER MERGING UX FIRST RUN
