e5b7f3e27bb4ed5be132915f1c104b35f24e7b7f
murzabek <murzabek@kolesa.kz>
2020-04-17 23:06:17 +0600

    Fix typo

--- app/src/main/java/org/covidwatch/android/presentation/MainActivity.kt
+++ app/src/main/java/org/covidwatch/android/presentation/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.covidwatch.android.R
 
-class MainActivitiy : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
