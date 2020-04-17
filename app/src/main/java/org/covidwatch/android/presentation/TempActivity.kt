5f8368949cfcc5caeb7c473726616904b14d4eb3
Nitin Kumar <nkumarcc@gmail.com>
2020-04-17 12:23:17 -0400

    Added init Contact Event Logging

--- app/src/main/java/org/covidwatch/android/presentation/TempActivity.kt
+++ app/src/main/java/org/covidwatch/android/presentation/TempActivity.kt
@@ -1,5 +1,6 @@
 package org.covidwatch.android.presentation
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,5 +11,22 @@ class TempActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_temp)
+        setContactEventLogging(true)
     }
+
+    private fun setContactEventLogging(enabled: Boolean) {
+
+        val application = this?.applicationContext ?: return
+        val sharedPref = application.getSharedPreferences(
+            application.getString(R.string.preference_file_key), Context.MODE_PRIVATE
+        ) ?: return
+        with(sharedPref.edit()) {
+            putBoolean(
+                application.getString(org.covidwatch.android.R.string.preference_is_contact_event_logging_enabled),
+                enabled
+            )
+            commit()
+        }
+    }
+
 }
