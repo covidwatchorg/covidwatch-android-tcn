f53cd561417d77087e440ab1e7de0ad7453b4c9d
jamesjmtaylor <james.jm.taylor@gmail.com>
2020-04-17 06:27:44 -0700

    Renamed TempActivity to MainActivity.

--- app/src/main/java/org/covidwatch/android/ble/BluetoothManager.kt
+++ app/src/main/java/org/covidwatch/android/ble/BluetoothManager.kt
@@ -15,7 +15,7 @@ import org.covidwatch.android.R
 import org.covidwatch.android.data.ContactEvent
 import org.covidwatch.android.data.ContactEventDAO
 import org.covidwatch.android.data.CovidWatchDatabase
-import org.covidwatch.android.presentation.TempActivity
+import org.covidwatch.android.presentation.MainActivitiy
 import org.tcncoalition.tcnclient.BluetoothService
 import org.tcncoalition.tcnclient.BluetoothService.LocalBinder
 import org.tcncoalition.tcnclient.cen.*
@@ -83,7 +83,7 @@ class BluetoothManagerImpl(
     private fun foregroundNotification(): Notification {
         createNotificationChannelIfNeeded()
 
-        val notificationIntent = Intent(app, TempActivity::class.java)
+        val notificationIntent = Intent(app, MainActivitiy::class.java)
         val pendingIntent = PendingIntent.getActivity(
             app, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT
         )
--- app/src/main/java/org/covidwatch/android/presentation/MainActivitiy.kt
+++ app/src/main/java/org/covidwatch/android/presentation/MainActivitiy.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.view.View
 import org.covidwatch.android.R
 
-class TempActivity : AppCompatActivity() {
+class MainActivitiy : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_temp)
+        setContentView(R.layout.activity_main)
     }
 
     override fun onResume() {
