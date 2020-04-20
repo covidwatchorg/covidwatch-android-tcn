d5ee58d0b1680affaf787e5991f38ebd73e6c349
Mark Byrne <3760653+markbyrne@users.noreply.github.com>
2020-04-20 17:13:22 -0500

    Added 4 basic tests for start page

--- app/src/androidTest/java/org/covidwatch/android/presentation/SplashFragmentTest.kt
+++ app/src/androidTest/java/org/covidwatch/android/presentation/SplashFragmentTest.kt
@@ -32,8 +32,6 @@ class SplashFragmentTest {
     @Test
     fun test_isSplashPageVisible() {
 
-
-
         // The "fragmentArgs" and "factory" arguments are optional.
 //        val fragmentArgs = Bundle().apply {
 //            putInt("selectedListItem", 0)
@@ -47,12 +45,64 @@ class SplashFragmentTest {
         // Check is displayed
         onView(withId(R.id.splashFragment)).check(matches(isDisplayed()))
 
-        // Check title text exists
-        onView(withId(R.id.textView)).check(matches(withText(R.string.app_name)))
+    }
 
+    @Test
+    fun test_LogoVisible() {
 
-
+        // Check Logo exists
+        onView(withId(R.id.imageView)).check(matches(isCompletelyDisplayed()))
 
     }
+
+    @Test
+    fun test_TitleVisible() {
+
+        // Check title text is displayed and matches app name
+        onView(withId(R.id.textView)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.textView)).check(matches(withText(R.string.app_name)))
+
+    }
+
+    @Test
+    fun test_subtitleVisible() {
+
+        // Check subtitle text is displayed and matches "Help your community stay safe, anonymously."
+        onView(withId(R.id.textView2)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.textView2)).check(matches(withText("Help your community stay safe, anonymously.")))
+
+    }
+
+    @Test
+    fun test_startButtonVisible() {
+
+        // Check start button is displayed and matches text "Start"
+        onView(withId(R.id.start_button)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.start_button)).check(matches(withText("Start")))
+
+    }
+
+    // Not yet built out
+//    @Test
+//    fun test_uiDisplayOrderMaintained() {
+//
+//        // Check that elements are displayed in correct order
+//        onView(withId(R.id.imageView)).check(isCompletelyAbove())
+//        onView(withId(R.id.textView))
+//        onView(withId(R.id.textView2))
+//        onView(withId(R.id.start_button))
+//
+//    }
+//
+//    @Test
+//    fun test_ElementsCentered() {
+//
+//        // Check that elements are centered
+//        onView(withId(R.id.imageView))
+//        onView(withId(R.id.textView))
+//        onView(withId(R.id.textView2))
+//        onView(withId(R.id.start_button)).check(matches(withText("Start")))
+//
+//    }
 
 }
