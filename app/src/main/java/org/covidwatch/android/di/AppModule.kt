5c9419a87483dde4ff723e23bbe9961d297ccc72
Nitin Kumar <nkumarcc@gmail.com>
2020-04-28 01:11:18 -0400

    Raw merge

--- app/src/main/java/org/covidwatch/android/di/AppModule.kt
+++ app/src/main/java/org/covidwatch/android/di/AppModule.kt
@@ -1,17 +1,21 @@
 package org.covidwatch.android.di
 
 import android.content.Context
-import okhttp3.OkHttpClient
 import org.covidwatch.android.CovidWatchTcnManager
+import androidx.work.WorkManager
+import okhttp3.OkHttpClient
 import org.covidwatch.android.NotificationFactory
 import org.covidwatch.android.data.CovidWatchDatabase
 import org.covidwatch.android.data.TestedRepositoryImpl
 import org.covidwatch.android.data.UserFlowRepositoryImpl
 import org.covidwatch.android.data.signedreport.firestore.SignedReportsUploader
+import org.covidwatch.android.data.contactevent.ContactEventsDownloader
 import org.covidwatch.android.domain.NotifyAboutPossibleExposureUseCase
 import org.covidwatch.android.domain.TestedRepository
 import org.covidwatch.android.domain.UserFlowRepository
+import org.covidwatch.android.presentation.home.EnsureTcnIsStartedUseCase
 import org.covidwatch.android.presentation.home.HomeViewModel
+import org.covidwatch.android.presentation.settings.SettingsViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -36,12 +40,32 @@ val appModule = module {
         )
     }
 
+    factory {
+        EnsureTcnIsStartedUseCase(
+            context = androidContext(),
+            tcnManager = get()
+        )
+    }
+
     viewModel {
+//        tcnDao = get(),
         HomeViewModel(
             userFlowRepository = get(),
             testedRepository = get(),
-            tcnDao = get()
+            ensureTcnIsStartedUseCase = get(),
+            contactEventDAO = get(),
+            contactEventsDownloader = get()
         )
+    }
+
+    factory {
+        val context = androidContext()
+        val workManager = WorkManager.getInstance(context)
+        ContactEventsDownloader(workManager)
+    }
+
+    viewModel {
+        SettingsViewModel(androidApplication())
     }
 
     single {
--- app/src/main/java/org/covidwatch/android/presentation/home/HomeViewModel.kt
+++ app/src/main/java/org/covidwatch/android/presentation/home/HomeViewModel.kt
@@ -1,110 +1,108 @@
 package org.covidwatch.android.presentation.home
 
-import android.bluetooth.BluetoothAdapter
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import org.covidwatch.android.R
-import org.covidwatch.android.data.TemporaryContactNumberDAO
+import org.covidwatch.android.data.ContactEvent
+import org.covidwatch.android.data.ContactEventDAO
+import org.covidwatch.android.data.contactevent.ContactEventsDownloader
 import org.covidwatch.android.domain.*
-import org.covidwatch.android.presentation.util.Event
+import org.covidwatch.android.presentation.util.getDistinct
 
 class HomeViewModel(
     private val userFlowRepository: UserFlowRepository,
     private val testedRepository: TestedRepository,
-    private val tcnDao: TemporaryContactNumberDAO
-) : ViewModel() {
-
-    private val bluetoothAdapter: BluetoothAdapter? by lazy { BluetoothAdapter.getDefaultAdapter() }
+    private val contactEventsDownloader: ContactEventsDownloader,
+    private val ensureTcnIsStartedUseCase: EnsureTcnIsStartedUseCase,
+    contactEventDAO: ContactEventDAO
+) : ViewModel(), EnsureTcnIsStartedPresenter {
 
     private val isUserTestedPositive: Boolean get() = testedRepository.isUserTestedPositive()
     private val _userTestedPositive = MutableLiveData<Unit>()
     val userTestedPositive: LiveData<Unit> get() = _userTestedPositive
 
-    private val _locationPermissionAction = MutableLiveData<Event<Unit>>()
-    val locationPermissionAction: LiveData<Event<Unit>> = _locationPermissionAction
+    private val _infoBannerState = MutableLiveData<InfoBannerState>()
+    val infoBannerState: LiveData<InfoBannerState> get() = _infoBannerState
 
-    private val _turnOnBluetoothAction = MutableLiveData<Event<Unit>>()
-    val turnOnBluetoothAction: LiveData<Event<Unit>> = _turnOnBluetoothAction
-
-    private val _banner = MutableLiveData<Banner>()
-    val banner: LiveData<Banner> get() = _banner
+    private val _warningBannerState = MutableLiveData<WarningBannerState>()
+    val warningBannerState: LiveData<WarningBannerState> get() = _warningBannerState
 
     private val _userFlow = MutableLiveData<UserFlow>()
     val userFlow: LiveData<UserFlow> get() = _userFlow
 
-    private val _potentialRiskAction = MutableLiveData<Event<Unit>>()
-    val potentialRiskAction: LiveData<Event<Unit>> get() = _potentialRiskAction
+    private val _isRefreshing = MediatorLiveData<Boolean>()
+    val isRefreshing: LiveData<Boolean> get() = _isRefreshing
 
-    fun setup() {
-        viewModelScope.launch(Dispatchers.IO) {
-            tcnDao.allSortedByDescTimestamp()
-                .map { it.fold(false) { infected, tcn -> infected || tcn.wasPotentiallyInfectious } }
-                .collect { potentiallyInfected ->
-                    if (potentiallyInfected && !isUserTestedPositive) {
-                        _banner.postValue(
-                            Banner.Warning(
-                                R.string.contact_alert_text,
-                                BannerAction.PotentialRisk
-                            )
-                        )
-                    } else if (isUserTestedPositive) {
-                        _banner.postValue(
-                            Banner.Warning(
-                                R.string.reported_alert_text,
-                                BannerAction.PotentialRisk
-                            )
-                        )
-                    }
+    private val hasPossiblyInteractedWithInfected: LiveData<Boolean> =
+        Transformations
+            .map(contactEventDAO.allSortedByDescTimestamp) { cenList ->
+                cenList.fold(initial = false) { isInfected: Boolean, event: ContactEvent ->
+                    isInfected || event.wasPotentiallyInfectious
                 }
+            }
+            .getDistinct()
+
+    private val interactedWithInfectedObserver =
+        Observer<Boolean> { hasPossiblyInteractedWithInfected ->
+            if (hasPossiblyInteractedWithInfected && !isUserTestedPositive) {
+                _warningBannerState.value = WarningBannerState.Visible(R.string.contact_alert_text)
+            }
         }
 
+    init {
+        hasPossiblyInteractedWithInfected.observeForever(interactedWithInfectedObserver)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        hasPossiblyInteractedWithInfected.removeObserver(interactedWithInfectedObserver)
+    }
+
+    override fun showLocationPermissionBanner() {
+        _infoBannerState.postValue(InfoBannerState.Visible(R.string.allow_location_access))
+    }
+
+    override fun showEnableBluetoothBanner() {
+        _infoBannerState.postValue(InfoBannerState.Visible(R.string.turn_bluetooth_on))
+    }
+
+    override fun hideBanner() {
+        _infoBannerState.postValue(InfoBannerState.Hidden)
+    }
+
+    fun onStart() {
         val userFlow = userFlowRepository.getUserFlow()
-        when (userFlow) {
-            !is Setup -> {
-                _locationPermissionAction.value = Event(Unit)
-                ensureBluetoothIsOn()
-                checkIfTestedPositive()
-            }
-            is FirstTimeUser -> {
-                userFlowRepository.updateFirstTimeUserFlow()
-            }
+        if (userFlow is FirstTimeUser) {
+            userFlowRepository.updateFirstTimeUserFlow()
         }
-
+        if (userFlow !is Setup) {
+            checkIfUserTestedPositive()
+            ensureTcnIsStarted()
+        }
         _userFlow.value = userFlow
     }
 
-    fun onBannerClicked() {
-        val bannerAction = _banner.value?.action ?: return
-
-        when (bannerAction) {
-            is BannerAction.TurnOnBluetooth -> {
-                _turnOnBluetoothAction.value = Event(Unit)
-            }
-            is BannerAction.PotentialRisk -> {
-                _potentialRiskAction.value = Event(Unit)
+    fun onRefreshRequested() {
+        val state = contactEventsDownloader.executePublicContactEventsRefresh()
+        _isRefreshing.addSource(state) {
+            _isRefreshing.value = !it
+            if (_isRefreshing.value == false) {
+                _isRefreshing.removeSource(state)
             }
         }
     }
 
-    fun bluetoothIsOn() {
-        _banner.value = Banner.Empty
-    }
-
-    private fun ensureBluetoothIsOn() {
-        if (bluetoothAdapter?.isEnabled == false) {
-            _banner.value = Banner.Info(R.string.turn_bluetooth_on, BannerAction.TurnOnBluetooth)
-        }
-    }
-
-    private fun checkIfTestedPositive() {
+    private fun checkIfUserTestedPositive() {
         if (isUserTestedPositive) {
             _userTestedPositive.value = Unit
+            _warningBannerState.value = WarningBannerState.Visible(R.string.reported_alert_text)
+        }
+    }
+
+    private fun ensureTcnIsStarted() {
+        viewModelScope.launch(Dispatchers.IO) {
+            ensureTcnIsStartedUseCase.execute(this@HomeViewModel)
         }
     }
 }
